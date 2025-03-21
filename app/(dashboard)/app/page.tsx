f5d00685cbf44d31a18076bc213821242287da5c
David Victor <david@davidvictor.me>
2025-03-20 23:29:24 -0700

    clean up

--- app/(dashboard)/app/page.tsx
+++ app/(dashboard)/app/page.tsx
@@ -1,5 +1,4 @@
 import { redirect } from 'next/navigation';
-// import { Settings } from '../settings';
 import { getUser } from '@/lib/db/queries';
 
 export default async function SettingsPage() {
